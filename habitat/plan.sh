79c6f3231962213cf0354434340adbc62efb9588
Nick Rycar <nickrycar@nicks-mbp.lan>
2019-09-06 13:57:22 -0700

    Updating deps

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,9 +1,14 @@
 pkg_name=sample-node-app
 pkg_origin=nrycar
 pkg_scaffolding="core/scaffolding-node"
-pkg_version="1.0.5"
+pkg_version="1.1.0"
+pkg_deps=(nrycar/libhelloworld)
 
 declare -A scaffolding_env
 
 # Define path to config file
 scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
+
+do_install() {
+  do_default_install
+}
