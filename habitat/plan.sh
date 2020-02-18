14500f3900fbf51e98f2d20c4f4ceec4c23e0fa6
Nick Rycar <nickrycar@Nicks-MacBook-Pro.local>
2020-02-18 08:50:02 -0800

    Version bump.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=sample-node-app
 pkg_origin=nrycar
 pkg_scaffolding="core/scaffolding-node"
-pkg_version="1.1.1"
+pkg_version="2.0.0"
 pkg_deps=(nrycar/libhelloworld)
 
 declare -A scaffolding_env
